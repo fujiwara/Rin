be7f583ba1198c4534427a8bfc99990ca155005f
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2015-04-24 14:32:19 +0900

    add -version flag

--- cmd/rin/main.go
+++ cmd/rin/main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	rin "github.com/fujiwara/Rin"
 )
 
+var (
+	version   string
+	buildDate string
+)
+
 func main() {
 	var (
-		config string
+		config      string
+		showVersion bool
 	)
 	flag.StringVar(&config, "config", "config.yaml", "config file path")
+	flag.StringVar(&config, "c", "config.yaml", "config file path")
 	flag.BoolVar(&rin.Debug, "debug", false, "enable debug logging")
+	flag.BoolVar(&rin.Debug, "d", false, "enable debug logging")
+	flag.BoolVar(&showVersion, "version", false, "show version")
+	flag.BoolVar(&showVersion, "v", false, "show version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println("version:", version)
+		fmt.Println("build:", buildDate)
+		return
+	}
+
 	err := rin.Run(config)
 	if err != nil {
 		panic(err)
